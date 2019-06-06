9fae9af314db0c0fe256da6830a6ef62fe853f71
wuting <wu.ting@navercorp.com>
2019-06-06 17:24:19 +0800

    modify

--- pages/index.vue
+++ pages/index.vue
@@ -34,20 +34,15 @@
           </template>
         </el-table-column>
       </el-table>
-      <div style="margin-top: 20px">
-        <el-button @click="toggleSelection([tableData[1], tableData[2]])">切换第二、第三行的选中状态</el-button>
-        <el-button @click="toggleSelection()">取消选择</el-button>
-      </div>
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :hide-on-single-page="true"
+        :total="totalCount"
+        :current-page="currentPage"
+        :page-size="pageSize">
+      </el-pagination>
     </div>
-
-    <el-pagination
-      background
-      layout="prev, pager, next"
-      :hide-on-single-page="true"
-      :total="totalCount"
-      :current-page="currentPage"
-      :page-size="pageSize">
-    </el-pagination>
   </div>
 </template>
 
@@ -118,7 +113,6 @@
 </script>
 
 <style>
-
   .dashboard-table {
     width: 100%;
     height: 100%;
